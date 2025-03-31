7edbfbca7c774288f41e996706c66c3771b2adf2
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-03-31 00:30:35 -0400

    Created Header Component

--- darwin/components/shared/header/index.tsx
+++ darwin/components/shared/header/index.tsx
@@ -1,5 +1,42 @@
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+
 const Header = () => {
-  return <>Header</>;
+  return (
+    <header className='w-full border-b'>
+      <div className='wrapper flex-between'>
+        <div className='flex-start'>
+          <Link href='/' className='flex-start'>
+            <Image
+              src='/images/logo.svg'
+              alt={`${APP_NAME}`}
+              width={50}
+              height={50}
+              priority={true}
+            />
+            <span className='hidden lg:block font-bold text-2xl ml-3'>
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className='space-x-2'>
+          <Button asChild variant='ghost'>
+            <Link href='/cart'>
+              <ShoppingCart />
+            </Link>
+          </Button>
+          <Button asChild variant='ghost'>
+            <Link href='/sign-in'>
+              <UserIcon /> Sign In
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
