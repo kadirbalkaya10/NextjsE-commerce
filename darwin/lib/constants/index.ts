125bd077246af34cff3b8fdba6233a5f61ba0cd8
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-03-31 00:03:21 -0400

    Added SERVER_URL  as global variable

--- darwin/lib/constants/index.ts
+++ darwin/lib/constants/index.ts
@@ -2,3 +2,5 @@ export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Darwin";
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   "A modern e-commerce app built with NextJs";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
