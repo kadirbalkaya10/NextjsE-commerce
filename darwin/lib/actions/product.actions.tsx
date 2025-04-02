f39cbba77ab6edbbb1a50322a4faa17c1ace4ad3
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-02 10:45:01 -0400

    Created Product actions to fetch data from database

---
+++ darwin/lib/actions/product.actions.tsx
@@ -0,0 +1,17 @@
+"use server";
+import { PrismaClient } from "@prisma/client";
+
+//Get latest Products
+
+export async function getLatestProducts() {
+  const prisma = new PrismaClient();
+
+  const data = await prisma.product.findMany({
+    take: 4,
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return data;
+}
