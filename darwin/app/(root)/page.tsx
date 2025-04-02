bda2fd3faca0d97e2547065721fd2b3c9230a052
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-01 22:47:02 -0400

    Created product list with basic layout to show the sample data

--- darwin/app/(root)/page.tsx
+++ darwin/app/(root)/page.tsx
@@ -1,5 +1,16 @@
+import sampleData from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
+
 const Homepage = () => {
-  return <>Homepage</>;
+  return (
+    <>
+      <ProductList
+        data={sampleData.products}
+        title='Newest Arrivals'
+        limit={4}
+      />
+    </>
+  );
 };
 
 export default Homepage;
---
+++ darwin/components/shared/product/product-list.tsx
@@ -0,0 +1,31 @@
+const ProductList = ({
+  data,
+  title,
+  limit,
+}: {
+  data: any;
+  title?: string;
+  limit?: number;
+}) => {
+  const limitedData = limit ? data.slice(0, limit) : data;
+
+  return (
+    <div className='my-10'>
+      <h2 className='h2-bold mb-4'>{title}</h2>
+      {data.length > 0 ? (
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+          {limitedData.map((product: any) => (
+            <div key={product}>{product.name}</div>
+          ))}
+        </div>
+      ) : (
+        <div className='flex flex-col items-center justify-center'>
+          <h2 className='h2-bold mb-4'>No Products Found</h2>
+          <p className='text-gray-500'>Please check back later.</p>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductList;
