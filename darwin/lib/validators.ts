80b9fbb0a7926742a38070df0f5cf4498a888e5a
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-04 08:23:20 -0400

    Created signUpFormSchema using zod to validate user credentials

--- darwin/lib/validators.ts
+++ darwin/lib/validators.ts
@@ -29,3 +29,17 @@ export const signInFormSchema = z.object({
   email: z.string().email("Invalid Email Address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
 });
+
+/// Schema for Signing Users up
+
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, "Name must be at least 3 characters."),
+    email: z.string().email("Invalid Email Address"),
+    password: z.string().min(6, "Password must be at least 6 characters"),
+    confirmPassword: z.string().min(6, "Confirm Password"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Password do not match",
+    path: ["confirmPassword"],
+  });
