883c54a4861c7989d9ac7e8d12b289d2a22503f5
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-04 09:55:24 -0400

    Customize Token With JWT Callback , to get user role , and user name if they sign Google provider.. etc , Get the name from email

--- darwin/auth.ts
+++ darwin/auth.ts
@@ -57,6 +57,9 @@ export const config = {
     async session({ session, user, trigger, token }: any) {
       // Set the user ID from the token
       session.user.id = token.sub;
+      session.user.role = token.role;
+      session.user.name = token.name;
+      console.log(token);
 
       // If there is an update , set the user name
       if (trigger === "update") {
@@ -64,6 +67,28 @@ export const config = {
       }
 
       return session;
+    },
+    async jwt({ token, user, trigger, session }: any) {
+      //Assign users field to token
+      if (user) {
+        token.role = user.role;
+
+        // If the user has no name then use email
+        if (user.name === "NO_NAME") {
+          token.name = user.email!.split("@")[0];
+          // Update database to reflect the token name
+          await prisma.user.update({
+            where: {
+              id: user.id,
+            },
+            data: {
+              name: token.name,
+            },
+          });
+        }
+      }
+
+      return token;
     },
   },
 } satisfies NextAuthConfig;
