1f40ab4909a6b93ca5a424427922f1e037eb6004
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-03 22:22:43 -0400

    Checking user if its logged in , if it is redirect to homepage

--- darwin/app/(auth)/sign-in/page.tsx
+++ darwin/app/(auth)/sign-in/page.tsx
@@ -10,12 +10,19 @@ import Link from "next/link";
 import Image from "next/image";
 import { APP_NAME } from "@/lib/constants";
 import CredentialsSignInForm from "@/components/credential-signin-form";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
-const SignInPage = () => {
+const SignInPage = async () => {
+  const session = await auth();
+
+  if (session) {
+    return redirect("/");
+  }
   return (
     <div className='w-full max-w-md mx-auto'>
       <Card>
