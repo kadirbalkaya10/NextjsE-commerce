bebf046cef9457e8135aa6f2417dbb545dc662a3
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-03 23:11:01 -0400

    Created User Button Component to sign in and sign out with dropdown , added sign out functionally

--- darwin/components/shared/header/menu.tsx
+++ darwin/components/shared/header/menu.tsx
@@ -14,6 +14,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
@@ -25,11 +26,7 @@ const Menu = () => {
             <ShoppingCart />
           </Link>
         </Button>
-        <Button asChild>
-          <Link href='/sign-in'>
-            <UserIcon /> Sign In
-          </Link>
-        </Button>
+        <UserButton />
       </nav>
       <nav className='md:hidden'>
         <Sheet>
@@ -43,11 +40,7 @@ const Menu = () => {
                   <ShoppingCartIcon /> Cart
                 </Link>
               </Button>
-              <Button asChild>
-                <Link href='/sign-in'>
-                  <UserIcon /> Sign In
-                </Link>
-              </Button>
+              <UserButton />
               <SheetDescription></SheetDescription>
             </SheetContent>
           </SheetTrigger>
