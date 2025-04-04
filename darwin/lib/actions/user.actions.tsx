2d857d59a5f22aec5bf1f43adc51696384a86068
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-03 22:19:35 -0400

    Correcting mis spelling

--- darwin/lib/actions/user.actions.tsx
+++ darwin/lib/actions/user.actions.tsx
@@ -1,4 +1,4 @@
-"user server";
+"use server";
 import { signInFormSchema } from "../validators";
 import { signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
