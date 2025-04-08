1772ede2d15ddf66e86b3bce5e8e07e9357b9f11
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-07 23:00:03 -0400

    Debugged for deployment

--- darwin/components/shared/header/menu.tsx
+++ darwin/components/shared/header/menu.tsx
@@ -1,11 +1,6 @@
 import { Button } from "@/components/ui/button";
 import ModeToggle from "./mode-toggle";
-import {
-  EllipsisVertical,
-  ShoppingCart,
-  ShoppingCartIcon,
-  UserIcon,
-} from "lucide-react";
+import { EllipsisVertical, ShoppingCart } from "lucide-react";
 import Link from "next/link";
 import {
   Sheet,
@@ -37,7 +32,7 @@ const Menu = () => {
               <ModeToggle />
               <Button asChild variant='ghost'>
                 <Link href='/cart'>
-                  <ShoppingCartIcon /> Cart
+                  <ShoppingCart /> Cart
                 </Link>
               </Button>
               <UserButton />
