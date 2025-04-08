4b781e8458b395730bad36595e27f1b59fb54db0
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-08 10:23:08 -0400

    Refactor the add to car btn , Created function to remove from card and ui changes

--- darwin/components/shared/product/add-to-cart.tsx
+++ darwin/components/shared/product/add-to-cart.tsx
@@ -1,12 +1,12 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
-import { CartItem } from "@/types";
-import { Plus } from "lucide-react";
+import { Cart, CartItem } from "@/types";
+import { Plus, Minus } from "lucide-react";
 import { toast } from "sonner";
-import { addItemToCart } from "@/lib/actions/cart-action";
+import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart-action";
 
-const AddToCart = ({ item }: { item: CartItem }) => {
+const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
 
   const handleAddToCart = async () => {
@@ -33,7 +33,33 @@ const AddToCart = ({ item }: { item: CartItem }) => {
     });
   };
 
-  return (
+  // Handle remove item from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    if (res.success) {
+      toast.success("", { description: res.message });
+    } else {
+      toast.warning("", { description: res.message });
+    }
+    return;
+  };
+
+  //Check if item is in cart
+
+  const existItem = cart?.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+        <Minus className='h-4 w-4' />
+      </Button>
+      <span className='px-2'>{existItem.qty}</span>
+      <Button type='button' variant='outline' onClick={handleAddToCart}>
+        <Plus className='h-4 w-4' />
+      </Button>
+    </div>
+  ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
       <Plus /> Add To Cart
     </Button>
