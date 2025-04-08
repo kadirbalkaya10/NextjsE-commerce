f2c0616ea2aaf61602e690d11d2f37c6ad5c8150
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-08 10:35:47 -0400

    Added useTransition hook from react for smooth UI experience when you adding or removing items from cart , if its on pending shows loader

--- darwin/components/shared/product/add-to-cart.tsx
+++ darwin/components/shared/product/add-to-cart.tsx
@@ -2,47 +2,53 @@
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 import { Cart, CartItem } from "@/types";
-import { Plus, Minus } from "lucide-react";
+import { Plus, Minus, Loader } from "lucide-react";
 import { toast } from "sonner";
 import { addItemToCart, removeItemFromCart } from "@/lib/actions/cart-action";
+import { useTransition } from "react";
 
 const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   const router = useRouter();
+  const [isPending, starTransition] = useTransition();
 
   const handleAddToCart = async () => {
-    const response = await addItemToCart(item);
+    starTransition(async () => {
+      const response = await addItemToCart(item);
 
-    if (!response.success) {
-      toast.error("We could not add to cart", {
+      if (!response.success) {
+        toast.error("We could not add to cart", {
+          description: response.message,
+          richColors: true,
+        });
+        return;
+      }
+      // Handle success add to cart
+      toast.success("", {
         description: response.message,
         richColors: true,
-      });
-      return;
-    }
-    // Handle success add to cart
-    toast.success("", {
-      description: response.message,
-      richColors: true,
 
-      action: {
-        label: "Go To Cart",
-        onClick: () => {
-          router.push("/cart");
+        action: {
+          label: "Go To Cart",
+          onClick: () => {
+            router.push("/cart");
+          },
         },
-      },
+      });
     });
   };
 
   // Handle remove item from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    starTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    if (res.success) {
-      toast.success("", { description: res.message });
-    } else {
-      toast.warning("", { description: res.message });
-    }
-    return;
+      if (res.success) {
+        toast.success("", { description: res.message });
+      } else {
+        toast.warning("", { description: res.message });
+      }
+      return;
+    });
   };
 
   //Check if item is in cart
@@ -52,16 +58,29 @@ const AddToCart = ({ cart, item }: { cart?: Cart; item: CartItem }) => {
   return existItem ? (
     <div>
       <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Minus className='h-4 w-4' />
+        )}
       </Button>
       <span className='px-2'>{existItem.qty}</span>
       <Button type='button' variant='outline' onClick={handleAddToCart}>
-        <Plus className='h-4 w-4' />
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Plus className='h-4 w-4' />
+        )}
       </Button>
     </div>
   ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+      {isPending ? (
+        <Loader className='w-4 h-4 animate-spin' />
+      ) : (
+        <Plus className='h-4 w-4' />
+      )}{" "}
+      Add To Cart
     </Button>
   );
 };
