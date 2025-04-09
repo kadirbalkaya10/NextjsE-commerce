434c5f3f7b5c2d41606e9eba846bdc813990206b
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-08 23:11:22 -0400

    Created Constant for paymentMethod and defaultPaymentMethod

--- darwin/lib/constants/index.ts
+++ darwin/lib/constants/index.ts
@@ -6,3 +6,9 @@ export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 export const LATEST_PRODUCTS_LIMIT =
   Number(process.env.NEXT_PUBLIC_LATEST_PRODUCTS_LIMIT) || 4;
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+  ? process.env.PAYMENT_METHODS.split(", ")
+  : ["Paypal", "Stripe"];
+export const DEFAULT_PAYMENT_METHOD =
+  process.env.DEFAULT_PAYMENT_METHOD || "Paypal";
