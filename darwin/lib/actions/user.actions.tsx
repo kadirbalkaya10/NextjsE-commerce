359e3353aa44f984252573525af347b268c3b3e3
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-08 21:08:59 -0400

    Created User Action to update user address

--- darwin/lib/actions/user.actions.tsx
+++ darwin/lib/actions/user.actions.tsx
@@ -1,10 +1,15 @@
 "use server";
-import { signInFormSchema, signUpFormSchema } from "../validators";
-import { signIn, signOut } from "@/auth";
+import {
+  shippingAddressSchema,
+  signInFormSchema,
+  signUpFormSchema,
+} from "../validators";
+import { auth, signIn, signOut } from "@/auth";
 import { isRedirectError } from "next/dist/client/components/redirect-error";
 import { hashSync } from "bcrypt-ts-edge";
 import { prisma } from "@/db/prisma";
 import { formatError } from "@/lib/utils";
+import { ShippingAddress } from "@/types";
 
 // Sign in user with credentials
 
@@ -81,4 +86,28 @@ export async function getUserById(userId: string) {
   });
   if (!user) throw new Error("User not found");
   return user;
+}
+
+// Update Users Address
+export async function updateUserAddress(data: ShippingAddress) {
+  try {
+    const session = await auth();
+
+    const currentUser = await prisma.user.findFirst({
+      where: {
+        id: session?.user?.id,
+      },
+    });
+    if (!currentUser) throw new Error("User not found");
+
+    const address = shippingAddressSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { address: address },
+    });
+    return { success: true, message: "User address update successfully" };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
 }
