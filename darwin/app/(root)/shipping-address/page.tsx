aad46efcefe12ee9b952cbbcb22000954fac0a0a
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-08 20:46:51 -0400

    Imported Shipping Addres Form Component to ShippinAddressPage

--- darwin/app/(root)/shipping-address/page.tsx
+++ darwin/app/(root)/shipping-address/page.tsx
@@ -4,6 +4,7 @@ import { getUserById } from "@/lib/actions/user.actions";
 import { Metadata } from "next";
 import { redirect } from "next/navigation";
 import { ShippingAddress } from "@/types";
+import ShippingAddressForm from "./shipping-address-form";
 
 export const metadata: Metadata = {
   title: "Shipping Address",
@@ -20,7 +21,11 @@ const ShippingAddressPage = async () => {
   if (!userId) throw new Error("No user id");
   const user = await getUserById(userId);
 
-  return <>Address</>;
+  return (
+    <>
+      <ShippingAddressForm address={user.address as ShippingAddress} />
+    </>
+  );
 };
 
 export default ShippingAddressPage;
