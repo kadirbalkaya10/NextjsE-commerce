75852ae9eaed417316a3ac87891aca1e517d7e12
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-08 21:09:24 -0400

    Created onSubmit function to handle form submit to update user address

--- darwin/app/(root)/shipping-address/shipping-address-form.tsx
+++ darwin/app/(root)/shipping-address/shipping-address-form.tsx
@@ -6,7 +6,7 @@ import { useTransition } from "react";
 import { ShippingAddress } from "@/types";
 import { shippingAddressSchema } from "@/lib/validators";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { ControllerRenderProps, useForm } from "react-hook-form";
+import { ControllerRenderProps, useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import {
   Form,
@@ -19,6 +19,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { ArrowRight, Loader } from "lucide-react";
+import { updateUserAddress } from "@/lib/actions/user.actions";
 
 const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
   const router = useRouter();
@@ -29,8 +30,21 @@ const ShippingAddressForm = ({ address }: { address: ShippingAddress }) => {
     defaultValues: address,
   });
 
-  const onSubmit = () => {
-    return;
+  const onSubmit: SubmitHandler<z.infer<typeof shippingAddressSchema>> = async (
+    values
+  ) => {
+    startTransition(async () => {
+      const res = await updateUserAddress(values);
+
+      if (!res.success) {
+        toast.error("", {
+          description: res.message,
+        });
+        return;
+      }
+
+      router.push("/payment-method");
+    });
   };
 
   return (
