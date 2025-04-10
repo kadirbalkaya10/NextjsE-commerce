0a60f8d974ea9d74801090ad41323f03e73c236e
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-10 15:58:48 -0400

    Created root for user file , and created layout for user pages

---
+++ darwin/app/user/layout.tsx
@@ -0,0 +1,38 @@
+import { APP_NAME } from "@/lib/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "@/components/shared/header/menu";
+import MainNav from "./main-nav";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className='flex flex-col p-4'>
+        <div className='border-b w-full mx-auto'>
+          <div className='flex item-center h-16 px-4'>
+            <Link href='/' className='w-22'>
+              <Image
+                src='/images/logo.svg'
+                height={48}
+                width={48}
+                alt={APP_NAME}
+              />
+            </Link>
+            {/* Main Nav */}
+            <MainNav className='mx-6' />
+            <div className='ml-auto item-center space-x-4'>
+              <Menu />
+            </div>
+          </div>
+        </div>
+        <div className='flex-1 space-y-4 p-8 pt-6 container mx-auto'>
+          {children}
+        </div>
+      </div>
+    </>
+  );
+}
