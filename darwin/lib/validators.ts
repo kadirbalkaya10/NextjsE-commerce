2205b354ea36719b37b912ed937f29b533e998df
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-11 19:22:33 -0400

    Created updateProductSchema using Zod to check typescript validation on update , Which is same with insertProductSchema just extend id

--- darwin/lib/validators.ts
+++ darwin/lib/validators.ts
@@ -24,6 +24,11 @@ export const insertProductSchema = z.object({
   price: currency,
 });
 
+// Schema for updating products
+export const updateProductSchema = insertProductSchema.extend({
+  id: z.string().min(1, "Id is required"),
+});
+
 // Schema for Signing Users in
 
 export const signInFormSchema = z.object({
