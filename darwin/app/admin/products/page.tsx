773bcb45c8b99a7feddc9c481c4107c1182b74d6
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-11 18:45:08 -0400

    Added Table component to show product info , Added Pagination component

--- darwin/app/admin/products/page.tsx
+++ darwin/app/admin/products/page.tsx
@@ -3,6 +3,16 @@ import { requireAdmin } from "@/lib/auth-guard";
 import Link from "next/link";
 import { getAllProducts } from "@/lib/actions/product.actions";
 import { formatCurrency, formatId } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import Pagination from "@/components/shared/pagination";
 
 export const metadata: Metadata = {
   title: "Admin Products",
@@ -29,7 +39,46 @@ const AdminProductPage = async (props: {
     <div className='space-y-2'>
       <div className='flex-between'>
         <h1 className='h2-bold'>Products</h1>
+        <Button asChild variant='default'>
+          <Link href='/admin/products/create'>Create a product</Link>
+        </Button>
       </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>NAME</TableHead>
+            <TableHead className='text-right mr-1'>PRICE</TableHead>
+            <TableHead>CATEGORY</TableHead>
+            <TableHead>STOCK</TableHead>
+            <TableHead>RATING</TableHead>
+            <TableHead className='w-[100px]'>ACTION</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.data.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{formatId(product.id)}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell className='text-right'>
+                {formatCurrency(product.price)}
+              </TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              <TableCell>{product.rating}</TableCell>
+              <TableCell className='flex gap-1'>
+                <Button asChild variant='outline' size='sm'>
+                  <Link href={`/admin/products/${product.id}`}>Edit</Link>
+                </Button>
+                {/* DELETE */}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {products?.totalPages && products.totalPages > 1 && (
+        <Pagination page={page} totalPages={products.totalPages}></Pagination>
+      )}
     </div>
   );
 };
