050d660d05c737fcedb3b78e7c80fc07125384a8
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-11 15:02:53 -0400

    Import auth to get session and pass isAdmin prop

--- darwin/app/(root)/order/[id]/page.tsx
+++ darwin/app/(root)/order/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getOrderById } from "@/lib/actions/order-actions";
 import { notFound } from "next/navigation";
 import OrderDetailsTable from "./order-details-table";
 import { ShippingAddress } from "@/types";
+import { auth } from "@/auth";
 
 export const metadata: Metadata = {
   title: "Order Details",
@@ -13,6 +14,8 @@ const OrderDetailPage = async (props: { params: Promise<{ id: string }> }) => {
   const order = await getOrderById(id);
   if (!order) notFound();
 
+  const session = await auth();
+
   return (
     <>
       <OrderDetailsTable
@@ -21,6 +24,7 @@ const OrderDetailPage = async (props: { params: Promise<{ id: string }> }) => {
           shippingAddress: order.shippingAddress as ShippingAddress,
         }}
         paypalClientId={process.env.PAYPAL_CLIENT_ID || "sb"}
+        isAdmin={session?.user?.role === "admin" || false}
       />
     </>
   );
