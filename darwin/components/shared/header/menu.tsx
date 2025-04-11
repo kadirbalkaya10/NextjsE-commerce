8e50120f24ff197bf2680fcf50e942329e50edf2
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-11 09:36:54 -0400

    Css adjustment

--- darwin/components/shared/header/menu.tsx
+++ darwin/components/shared/header/menu.tsx
@@ -13,7 +13,7 @@ import UserButton from "./user-button";
 
 const Menu = () => {
   return (
-    <div className='flex justify-end gap-3'>
+    <div className='flex justify-end items-center gap-3'>
       <nav className='hidden md:flex w-full max-w-xs gap-1'>
         <ModeToggle />
         <Button asChild variant='ghost'>
