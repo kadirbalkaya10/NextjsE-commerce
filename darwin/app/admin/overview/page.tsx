d4661a5cd39382345a9196bb524896b807f38bd5
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-11 11:18:25 -0400

    import requireAdmin function from auth-guard file to protect admin routes

--- darwin/app/admin/overview/page.tsx
+++ darwin/app/admin/overview/page.tsx
@@ -14,12 +14,14 @@ import { BadgeDollarSign, Barcode, CreditCard, Users } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
 import Charts from "./chart";
+import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
 };
 
 const AdminOverviewPage = async () => {
+  await requireAdmin();
   const session = await auth();
 
   if (session?.user?.role !== "admin")
