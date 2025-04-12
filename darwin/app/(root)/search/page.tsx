528e93eb4da02e7771b2d3ab73c3abd155cb706e
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-12 19:54:26 -0400

    Created search page to view products with query and sorting opts, or all products

--- darwin/app/(root)/search/page.tsx
+++ darwin/app/(root)/search/page.tsx
@@ -1,5 +1,48 @@
-const SearchPage = () => {
-  return <>Search page</>;
+import ProductCard from "@/components/shared/product/product-card";
+import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/lib/actions/product.actions";
+
+const SearchPage = async (props: {
+  searchParams: Promise<{
+    query?: string;
+    category?: string;
+    price?: string;
+    rating?: string;
+    sort?: string;
+    page?: string;
+  }>;
+}) => {
+  const {
+    query = "all",
+    category = "all",
+    price = "all",
+    rating = "all",
+    sort = "newest",
+    page = "1",
+  } = await props.searchParams;
+
+  const products = await getAllProducts({
+    query,
+    category,
+    price,
+    rating,
+    sort,
+    page: Number(page),
+  });
+
+  return (
+    <div className='grid md:grid-cols-5 md:gap-5'>
+      <div className='filter-links'>{/* Filters */}</div>
+      <div className='space-y-4 md:col-span-4'>
+        <div className='grid grid-cols-1 gap-4 md:grid-cols-3'>
+          {products.data.length === 0 && "No Products Found"}
+          {products.data.map((x) => (
+            <ProductCard key={x.id} product={x} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchPage;
