ce1e383dc9a963b8267156f51c2fb498c33fe305
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-12 17:46:09 -0400

    Created queryFilter for getAllProduct action

--- darwin/lib/actions/product.actions.tsx
+++ darwin/lib/actions/product.actions.tsx
@@ -5,6 +5,7 @@ import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from "../constants";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 import { insertProductSchema, updateProductSchema } from "../validators";
+import { Prisma } from "@prisma/client";
 
 //Get latest Products
 
@@ -42,12 +43,23 @@ export async function getAllProducts({
   page,
   category,
 }: {
-  query: string;
   limit?: number;
   page: number;
+  query: string;
   category?: string;
 }) {
+  // Query filter
+  const queryFilter: Prisma.ProductWhereInput =
+    query && query !== "all"
+      ? {
+          name: {
+            contains: query,
+            mode: "insensitive",
+          } as Prisma.StringFilter,
+        }
+      : {};
   const data = await prisma.product.findMany({
+    where: { ...queryFilter },
     orderBy: { createdAt: "desc" },
     skip: (page - 1) * limit,
     take: limit,
