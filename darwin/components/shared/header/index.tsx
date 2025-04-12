17edc3e182bcdcaa2ce965876e928e008df30546
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-12 18:37:40 -0400

    Category-drawer component created

--- darwin/components/shared/header/index.tsx
+++ darwin/components/shared/header/index.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 import { APP_NAME } from "@/lib/constants";
 import Menu from "./menu";
+import CategoryDrawer from "./category-drawer";
 
 const Header = () => {
   return (
     <header className='w-full border-b'>
       <div className='wrapper flex-between'>
         <div className='flex-start'>
-          <Link href='/' className='flex-start'>
+          <CategoryDrawer />
+          <Link href='/' className='flex-start ml-2'>
             <Image
               src='/images/logo.svg'
               alt={`${APP_NAME}`}
