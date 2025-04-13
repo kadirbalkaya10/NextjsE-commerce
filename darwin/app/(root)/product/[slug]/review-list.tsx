24ac68fb98625451a84f917b8fc7d327b911562b
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-13 09:56:56 -0400

    Created review-list component , takes props userId,ProductId,ProductSlug made basic layout and logic to show reviews

---
+++ darwin/app/(root)/product/[slug]/review-list.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { Review } from "@/types";
+import Link from "next/link";
+import { useState } from "react";
+
+const ReviewList = ({
+  userId,
+  productId,
+  productSlug,
+}: {
+  userId: string;
+  productId: string;
+  productSlug: string;
+}) => {
+  const [reviews, setReviews] = useState<Review[]>([]);
+  return (
+    <div className='space-y-4'>
+      {reviews.length === 0 && <div>No reviews yet</div>}
+      {userId ? (
+        <>{/* Review form her */}</>
+      ) : (
+        <div>
+          Please
+          <Link
+            href={`/sign-in?callbackUrl=/product/${productSlug}`}
+            className='text-blue-700 px-2'>
+            sign in
+          </Link>
+          to write a review
+        </div>
+      )}
+      <div className='flex flex-col gap-3'>{/* Reviews here */}</div>
+    </div>
+  );
+};
+
+export default ReviewList;
