a93c10d898aa154f68cfcb968108dafa6ce8ffec
Kadir Balkaya <122707602+kadirbalkaya10@users.noreply.github.com>
2025-04-14 14:53:56 -0400

    Added sendPurchaseReceipt function updateOrderToPaid , now afterpurchase is made and set isPaid ture sends email receipts

--- darwin/lib/actions/order-actions.ts
+++ darwin/lib/actions/order-actions.ts
@@ -7,11 +7,12 @@ import { getMyCart } from "./cart-action";
 import { getUserById } from "./user.actions";
 import { insertOrderSchema } from "../validators";
 import { prisma } from "@/db/prisma";
-import { CartItem, PaymentResult } from "@/types";
+import { CartItem, PaymentResult, ShippingAddress } from "@/types";
 import { paypal } from "../paypal/paypal";
 import { revalidatePath } from "next/cache";
 import { PAGE_SIZE } from "../constants";
 import { Prisma } from "@prisma/client";
+import { sendPurchaseReceipt } from "@/email";
 
 //Create order and orderItems
 
@@ -262,6 +263,14 @@ export async function updateOrderToPaid({
   });
 
   if (!updatedOrder) throw new Error("Order not found");
+
+  sendPurchaseReceipt({
+    order: {
+      ...updatedOrder,
+      shippingAddress: updatedOrder.shippingAddress as ShippingAddress,
+      paymentResult: updatedOrder.paymentResult as PaymentResult,
+    },
+  });
 }
 
 //Get User`s Orders
